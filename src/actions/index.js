dea974573e555369cdf3397a7e359745b4393203
horlard <oladipuposaulih@gmail.com>
2019-06-18 19:05:50 +0100

    created action creators for each stream actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,5 +12,40 @@ export const Signout = () => {
     }
 }
 export const createStream = formValues => async  dispatch => {
-    Streams.post('/streams',formValues)
+    const response= await Streams.post('/streams',formValues);
+
+    dispatch({
+        type : 'CREATE_STREAM',
+        payload : response.data
+    })
 }
+export const fetchStreams = () => async dispatch => {
+    const response = await Streams.get('/streams');
+    dispatch ({
+        type : 'FETCH_STREAMS',
+        payload : response.data
+    })
+}
+export const fetchStream =(id) => async dispatch => {
+    const response = await Streams.get(`/streams/${id}`);
+
+    dispatch({
+        type : 'FETCH_STREAM',
+        payload : response.data
+    })
+}
+export const deleteStream = (id) => async dispatch => {
+   await Streams.delete(`/streams/${id}`);
+    dispatch ({
+        type : 'DELETE_STREAM',
+        payload : id
+    })
+}
+export const editStream = (id,formValues) => async dispatch => {
+    const response=await Streams.put(`/streams/${id}`,formValues);
+    dispatch ({
+        type : 'EDIT_STREAM',
+        payload: response.data
+    })
+}
+
