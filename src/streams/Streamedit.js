bec43d101e1f959cfadf7691a3740afb9a640d59
horlard <oladipuposaulih@gmail.com>
2019-06-29 10:27:45 +0100

    imported streamForm into streamEdit and passed in the editStream action crreator

--- src/streams/Streamedit.js
+++ src/streams/Streamedit.js
@@ -1,8 +1,27 @@
 import React from 'react';
-
-
-const Streamedit = () => {
-    return <div>Streamedit</div>
+import {connect} from 'react-redux';
+import _ from 'lodash';
+import {fetchStream,editStream} from '../actions';
+import StreamForm from './streamForm';
+class Streamedit extends React.Component {
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id)
+    }
+    onSubmit = (formValues) => {
+        this.props.editStream(this.props.match.params.id,formValues);
+        console.log(formValues);
+    }
+    render() {
+        return (
+            <div>
+                <h3>Edit a Stream</h3>
+                <StreamForm onSubmit={this.onSubmit} initialValues={_.pick(this.props.stream,'title','description')}/>
+            </div>
+        )
+    }
+    
 }
-
-export default Streamedit;
+const mapStateToProps = (state,ownProps) => {
+    return {stream:state.streams[ownProps.match.params.id]}
+}
+export default connect(mapStateToProps,{fetchStream,editStream})(Streamedit);
