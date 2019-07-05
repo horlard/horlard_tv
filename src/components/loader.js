a883db325479582b48afe0a3108b210c5c0db5df
horlard <oladipuposaulih@gmail.com>
2019-07-05 16:52:44 +0100

    created a loader component to show a spinner when the user is not signed in

---
+++ src/components/loader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Loader = () => {
+    return (
+        <div>
+            <div class="ui small active inline loader"></div>
+            Please sign in with your google account
+                       
+        </div>
+    )
+}
+
+export default Loader;
